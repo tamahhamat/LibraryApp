d8d31675f2cd146c45c7d691df7f1185443471a8
tamahhamat <newman.tamah@gmail.com>
2024-05-13 22:52:19 +0200

    book model

---
+++ BackEnd/Models/bookModel.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(ISBN, title, author, genre) {
+      this.ISBN = ISBN;
+      this.title = title;
+      this.author = author;
+      this.genre = genre;
+    }
+  }
+  
+  module.exports = Book;
