06c3ba0b9a6ee0f3fc4fbc3607e84c0613831c60
tamahhamat <newman.tamah@gmail.com>
2024-05-13 22:52:36 +0200

    user model

---
+++ BackEnd/Models/userModel.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, username, password) {
+      this.id = id;
+      this.username = username;
+      this.password = password;
+      // Add more fields as needed, such as email, role, etc.
+    }
+  }
+  
+  module.exports = User;
