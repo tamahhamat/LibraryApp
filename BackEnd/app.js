054c34f4c6b1a93f97bc18efda0f996743fef9ae
tamahhamat <newman.tamah@gmail.com>
2024-05-13 22:42:29 +0200

    app.js set up and testing first route

---
+++ BackEnd/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set up views directory
+app.set('views', path.join(__dirname, 'FrontEnd', 'views'));
+app.set('view engine', 'html');
+
+// Serve static files from the FrontEnd directory
+app.use(express.static(path.join(__dirname, 'FrontEnd')));
+
+// Import routes
+const appRoutes = require('./Routes/appRoutes');
+
+
+// Mount routes
+app.use('/', appRoutes);
+
+
+
+const port = process.env.PORT || 800; 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
