5390106290525dd1773a2b2c41ec41947ad232ab
tamahhamat <newman.tamah@gmail.com>
2024-05-14 20:13:26 +0200

    start of login query, check for username and password in database

--- BackEnd/Models/queries.js
+++ BackEnd/Models/queries.js
@@ -1,5 +1,9 @@
 const pool = require('../database');
+const User = require('./userModel');
 
+
+
+//search books
 async function searchBooks(query) {
   const searchQuery = `
     SELECT * 
@@ -10,6 +14,31 @@ async function searchBooks(query) {
   return rows;
 }
 
+
+
+
+
+// Get user by username
+async function getUserByUsername(username) {
+  const query = 'SELECT * FROM users WHERE username = $1';
+  const result = await pool.query(query, [username]);
+
+  if (result.rows.length === 0) {
+      return null; // Return null if user not found
+  }
+
+  const { id, username: fetchedUsername, password } = result.rows[0];
+  return new User(id, fetchedUsername, password);
+}
+
+
+
+
 module.exports = {
-  searchBooks
+  searchBooks,
+  getUserByUsername
 };
+
+
+
+
