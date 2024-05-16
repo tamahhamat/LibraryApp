1c61941ddd00ac595c5710866c23ff730ff1d3a2
tamahhamat <newman.tamah@gmail.com>
2024-05-16 22:18:19 +0200

    add book route

--- BackEnd/Routes/authRoutes.js
+++ BackEnd/Routes/authRoutes.js
@@ -20,6 +20,18 @@ router.get('/editBook', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'FrontEnd', 'views', 'editBook.html'));
 });
 
+// Add a book
+router.post('/addBook', async (req, res) => {
+    const { ISBN, title, author, genre } = req.body;
+    try {
+        await addBook({ ISBN, title, author, genre });
+        res.status(200).send('Book added successfully');
+    } catch (error) {
+        console.error('Error adding book:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 
 module.exports = router;
