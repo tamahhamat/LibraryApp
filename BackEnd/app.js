da2562f21e141de86a22f2036b8b414f85bb513c
tamahhamat <newman.tamah@gmail.com>
2024-07-11 22:29:53 +0200

    added cors, commented out for testing

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,31 +1,47 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 800; 
 
 
-// Serve static files from the FrontEnd directory
-app.use(express.static(path.join(__dirname, 'FrontEnd')));
-
-// Set MIME type for JavaScript files in the Public directory
-app.use('/Public', (req, res, next) => {
-    if (req.url.endsWith('.js')) {
-      res.setHeader('Content-Type', 'text/javascript');
-    }
-    next();
-});
-
-// Set up views directory
-app.set('views', path.join(__dirname, 'FrontEnd', 'views'));
-app.set('view engine', 'html');
-
+app.use(cors({
+  origin: 'http://127.0.0.1:5501', // Allow requests from this origin
+  methods: ['GET', 'POST'], // Allow only GET and POST requests
+  credentials: true, // Allow cookies and authorization headers
+}));
 
 // body-parser middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// Serve static files from the FrontEnd directory
+//app.use(express.static(path.join(__dirname, 'FrontEnd')));
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+
+
+// // Set MIME type for JavaScript files in the Public directory
+// app.use('/Public', (req, res, next) => {
+//     if (req.url.endsWith('.js')) {
+//       res.setHeader('Content-Type', 'text/javascript');
+//     }
+//     next();
+// });
+
+
+// Set MIME type for JavaScript files in the Public directory
+app.use('/public', express.static(path.join(__dirname, '../Frontend/Public')));
+
+
+// // Set up views directory
+// app.set('views', path.join(__dirname, 'FrontEnd', 'views', 'public'));
+// app.set('view engine', 'html');
 
 
 // Import routes
@@ -42,7 +58,7 @@ app.use(authRoutes);
 
 
 
-const port = process.env.PORT || 800; 
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
