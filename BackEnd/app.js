3ffc263db8e87a4adfe60ca5f5409e3c3194c619
tamahhamat <newman.tamah@gmail.com>
2024-07-13 23:06:49 +0200

    adjustments

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,15 +4,16 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 800; 
 
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5501', // Allow requests from this origin
-  methods: ['GET', 'POST'], // Allow only GET and POST requests
-  credentials: true, // Allow cookies and authorization headers
+  origin: 'http://127.0.0.1:5501', // allow requests from this origin
+  methods: ['GET', 'POST'], // allow GET and POST 
+  credentials: true, 
 }));
 
 // body-parser middleware to parse request bodies
@@ -20,28 +21,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     styleSrc: ["'self'", 'https://fonts.googleapis.com'],
+//     fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+//     imgSrc: ["'self'", 'data:', 'https://your-image-source.com'],
+//     scriptSrc: ["'self'", 'https://cdn.jsdelivr.net'], // Add any other script sources
+//     connectSrc: ["'self'", 'https://fonts.googleapis.com'], // For fetching fonts and other external resources
+//     objectSrc: ["'none'"],
+//     upgradeInsecureRequests: []
+//   },
+// }));
+
+
 // Serve static files from the FrontEnd directory
-//app.use(express.static(path.join(__dirname, 'FrontEnd')));
-app.use(express.static(path.join(__dirname, '../Frontend')));
+app.use(express.static(path.join(__dirname, '../Fronten/views')));
 
-
-
-// // Set MIME type for JavaScript files in the Public directory
-// app.use('/Public', (req, res, next) => {
-//     if (req.url.endsWith('.js')) {
-//       res.setHeader('Content-Type', 'text/javascript');
-//     }
-//     next();
-// });
-
-
-// Set MIME type for JavaScript files in the Public directory
-app.use('/public', express.static(path.join(__dirname, '../Frontend/Public')));
-
-
-// // Set up views directory
-// app.set('views', path.join(__dirname, 'FrontEnd', 'views', 'public'));
-// app.set('view engine', 'html');
 
 
 // Import routes
@@ -55,8 +51,6 @@ app.use(appRoutes);
 app.use('/library', appRoutes);
 app.use(userRoutes);
 app.use(authRoutes);
-
-
 
 
 
