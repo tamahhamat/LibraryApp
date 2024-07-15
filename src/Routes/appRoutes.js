be1ab802f6140bdf8ed4d05e7d4616ea737b1669
tamahhamat <newman.tamah@gmail.com>
2024-07-15 21:03:52 +0200

    ythis is it!

--- src/Routes/appRoutes.js
+++ src/Routes/appRoutes.js
@@ -9,7 +9,7 @@ const { generateToken, storeToken } = require('../utils');
 
 // Route to serve home.html
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'FrontEnd', 'views', 'home.html'));
+    res.sendFile(path.join(__dirname,  'FrontEnd', 'views', 'home.html'));
 });
 
 
@@ -41,7 +41,7 @@ router.get('/search', async (req, res) => {
 
 // get librarian page
 router.get('/librarian', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'Frontend', 'views', 'librarianPage.html'));
+  res.sendFile(path.join(__dirname, 'Frontend', 'views', 'librarianPage.html'));
 });
 
 
