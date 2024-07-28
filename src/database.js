ecf28bba16b1e9e55450c4c9114b7c7058805bba
tamahhamat <newman.tamah@gmail.com>
2024-07-28 16:42:13 +0200

    db

--- src/database.js
+++ src/database.js
@@ -4,10 +4,13 @@ require('dotenv').config();
 // new Pool instance using environment variables
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 
